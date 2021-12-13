a44d5325506a2558926f31e397ffdf4302f0ff83
Volodymyr Mykhailyk <712680+volodymyr-mykhailyk@users.noreply.github.com>
2021-12-13 11:18:20 +0200

    Day13 part2

--- cmd/day13/main.go
+++ cmd/day13/main.go
@@ -13,6 +13,12 @@ func main() {
 	sheet := vision.ActivationSheet(lines)
 
 	fmt.Printf("Rading activation sheet with %v points\n", vision.VisibleActivationDots(sheet))
-	sheet = vision.FoldActivationSheet(sheet, instructions[0])
-	fmt.Printf("Dots after first fold: %v\n", vision.VisibleActivationDots(sheet))
+	firstFold := vision.FoldActivationSheet(sheet, instructions[0])
+	fmt.Printf("Dots after first fold: %v\n", vision.VisibleActivationDots(firstFold))
+
+	for _, instr := range instructions {
+		sheet = vision.FoldActivationSheet(sheet, instr)
+	}
+	fmt.Printf("Activation sheet:\n")
+	fmt.Printf("%v\n", sheet.Presentation())
 }
